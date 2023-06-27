50be6efb11aab8dcdbdbb6a73a5e0c3285a5fbef
psalmuelle <bjoestores@gmail.com>
2023-06-27 18:40:22 +0100

    feat: navbar

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,18 +1,21 @@
-import Link from "next/link"
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer>
-        <div>
-            <p>Erinle Samuel</p>
-            <p>Software Engineer(Web)</p>
-        </div>
-        <p>&copy; 2023</p>
-        <Link href='https://github.com/psalmuelle/psalmuelle.github.io#readme' target='_blank'>
-            #Design Attributes
-        </Link>
+    <footer className='relative border-t border-white mt-20 py-10 pt-8 flex justify-center items-center flex-col text-center'>
+      <div>
+        <p>Erinle Samuel</p>
+        <p className="text-sm">Software Engineer (Web)</p>
+        <p className="text-sm opacity-80">&copy; {new Date().getFullYear()}</p>
+      </div>
+      <Link
+        href='https://github.com/psalmuelle/psalmuelle.github.io#readme'
+        target='_blank'
+        className='absolute bottom-3 right-6 text-blue-400 text-sm'>
+        #Design Attributes
+      </Link>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -31,7 +31,7 @@ const NavLink = ({ title, href, icon, activeLink, hideTitle }) => {
 const SocialLink = ({ href, title, icon, followers, hideTitle }) => {
   return (
     <Link
-      className={`flex justify-between items-center p-2 rounded-md last-of-type:justify-center hover:bg-black ${
+      className={`flex justify-between items-center p-2 rounded-lg last-of-type:justify-center hover:bg-black ${
         hideTitle ? "block w-fit p-3" : ""
       }`}
       href={href}
@@ -48,21 +48,26 @@ const SocialLink = ({ href, title, icon, followers, hideTitle }) => {
 
 const Nav = ({ activeLink }) => {
   const [hideNav, setHideNav] = useState(false);
-
+  const [mobileNav, setMobileNav] = useState(false);
   const HideNavTexts = () => {
     setHideNav((prev) => !prev);
   };
 
+  const HideMobileNav = () => {
+    setMobileNav((prev) => !prev);
+  };
+
   return (
     <nav
-      className={`glassmorphism text-white p-4 bg-[rgba(116,29,216,0.9)] w-64 ${
+      className={`glassmorphism text-white p-4 max-xl:p-2 bg-[rgba(116,29,216,0.9)] w-64 ${
         hideNav ? "w-fit" : ""
-      } `}>
-      <div className='max-xl:hidden'>
+      } mobile_nav`}>
+      <div className=''>
+        {/* DeskTop Navigation Menu */}
         <div
           className={`flex justify-between items-center ${
             hideNav ? "flex-col-reverse gap-6" : ""
-          }`}>
+          } max-xl:hidden`}>
           <div className='border-2 border-white px-2 py-3.5 rounded-full'>
             <Image
               className='invert'
@@ -77,87 +82,107 @@ const Nav = ({ activeLink }) => {
           </div>
         </div>
 
-        <div className='mt-10 flex justify-center flex-col gap-2'>
-          <NavLink
-            title={"Home"}
-            activeLink={activeLink}
-            href={"/"}
-            icon={<FiHome size={hideNav ? 20 : ""} />}
-            hideTitle={hideNav}
-          />
-
-          <NavLink
-            title={"Projects"}
-            activeLink={activeLink}
-            href={"/projects"}
-            icon={<GrProjects size={hideNav ? 20 : ""} className='invert' />}
-            hideTitle={hideNav}
-          />
-
-          <NavLink
-            title={"Blog"}
-            activeLink={activeLink}
-            href={"/blog"}
-            icon={<BsPencilSquare size={hideNav ? 20 : ""} />}
-            hideTitle={hideNav}
-          />
-
-          <NavLink
-            title={"About"}
-            href={"/about"}
-            icon={<AiOutlineInfoCircle size={hideNav ? 20 : ""} />}
-            activeLink={activeLink}
-            hideTitle={hideNav}
-          />
-
-          <NavLink
-            title={"Contact"}
-            activeLink={activeLink}
-            href={"/contact"}
-            icon={<GrContact className='invert' size={hideNav ? 20 : ""} />}
-            hideTitle={hideNav}
-          />
+        {/* Mobile Navigation Menu */}
+        <div className={`flex justify-between items-center xl:hidden`}>
+          <div className='border-2 border-white px-2 py-3 rounded-full'>
+            <Image
+              className='invert'
+              src={"/es1.png"}
+              alt='Logo'
+              width={24}
+              height={24}
+            />
+          </div>
+          <div
+            onClick={HideMobileNav}
+            className='p-2 cursor-pointer active_btn'>
+            {mobileNav ? <MdClose size={24} /> : <FiMenu size={24} />}
+          </div>
         </div>
 
-        <div className='glassmorphism bg-opacity-50 bg-gray-50 mt-6 mb-2 text- rounded-md py-4 px-2 flex justify-center flex-col gap-2'>
-          <SocialLink
-            href={"https://www.linkedin.com/in/erinle-samuel/"}
-            title={"LinkedIn"}
-            icon={<AiOutlineLinkedin size={hideNav ? 20 : ""} />}
-            followers={"200"}
-            hideTitle={hideNav}
-          />
+        <div className={` ${mobileNav ? "max-xl:block" : "max-xl:hidden"}`}>
+          <div className={`mt-10 flex justify-center flex-col gap-2`}>
+            <NavLink
+              title={"Home"}
+              activeLink={activeLink}
+              href={"/"}
+              icon={<FiHome size={hideNav ? 20 : ""} />}
+              hideTitle={hideNav}
+            />
 
-          <SocialLink
-            href={"https://github.com/psalmuelle"}
-            title={"GitHub"}
-            icon={<AiOutlineGithub size={hideNav ? 20 : ""} />}
-            followers={"15"}
-            hideTitle={hideNav}
-          />
+            <NavLink
+              title={"Projects"}
+              activeLink={activeLink}
+              href={"/projects"}
+              icon={<GrProjects size={hideNav ? 20 : ""} className='invert' />}
+              hideTitle={hideNav}
+            />
 
-          <SocialLink
-            href={"https://medium.com/@erinle-sam"}
-            title={"Medium"}
-            icon={<BsMedium size={hideNav ? 20 : ""} />}
-            followers={"50"}
-            hideTitle={hideNav}
-          />
+            <NavLink
+              title={"Blog"}
+              activeLink={activeLink}
+              href={"/blog"}
+              icon={<BsPencilSquare size={hideNav ? 20 : ""} />}
+              hideTitle={hideNav}
+            />
 
-          <SocialLink
-            href={"https://twitter.com/erinle_sam"}
-            title={"Twitter"}
-            icon={<FiTwitter size={hideNav ? 20 : ""} />}
-            followers={"50"}
-            hideTitle={hideNav}
-          />
+            <NavLink
+              title={"About"}
+              href={"/about"}
+              icon={<AiOutlineInfoCircle size={hideNav ? 20 : ""} />}
+              activeLink={activeLink}
+              hideTitle={hideNav}
+            />
 
-          <SocialLink
-            href={"mailto:psalmuelle1@gmail.com"}
-            title={"Email"}
-            icon={<AiOutlineSend size={hideNav ? 20 : ""} />}
-            hideTitle={hideNav}
-          />
+            <NavLink
+              title={"Contact"}
+              activeLink={activeLink}
+              href={"/contact"}
+              icon={<GrContact className='invert' size={hideNav ? 20 : ""} />}
+              hideTitle={hideNav}
+            />
+          </div>
+
+          <div className='glassmorphism bg-opacity-50 bg-gray-50 mt-6 mb-2 text- rounded-md py-4 px-2 flex justify-center flex-col gap-2'>
+            <SocialLink
+              href={"https://www.linkedin.com/in/erinle-samuel/"}
+              title={"LinkedIn"}
+              icon={<AiOutlineLinkedin size={hideNav ? 20 : ""} />}
+              followers={"200"}
+              hideTitle={hideNav}
+            />
+
+            <SocialLink
+              href={"https://github.com/psalmuelle"}
+              title={"GitHub"}
+              icon={<AiOutlineGithub size={hideNav ? 20 : ""} />}
+              followers={"15"}
+              hideTitle={hideNav}
+            />
+
+            <SocialLink
+              href={"https://medium.com/@erinle-sam"}
+              title={"Medium"}
+              icon={<BsMedium size={hideNav ? 20 : ""} />}
+              followers={"50"}
+              hideTitle={hideNav}
+            />
+
+            <SocialLink
+              href={"https://twitter.com/erinle_sam"}
+              title={"Twitter"}
+              icon={<FiTwitter size={hideNav ? 20 : ""} />}
+              followers={"50"}
+              hideTitle={hideNav}
+            />
+
+            <SocialLink
+              href={"mailto:psalmuelle1@gmail.com"}
+              title={"Email"}
+              icon={<AiOutlineSend size={hideNav ? 20 : ""} />}
+              hideTitle={hideNav}
+            />
+          </div>
         </div>
       </div>
     </nav>
